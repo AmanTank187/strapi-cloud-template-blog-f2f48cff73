83998ebdeea282414fe60ae84e24d4ac0e4956d3
Aman Tank <aman.tank@bridge-u.com>
2023-12-14 10:01:16 +0000

    Add config plugin

---
+++ config/plugins.js
@@ -0,0 +1,43 @@
+// ./config/plugins.js
+'use strict';
+
+module.exports = {
+  'preview-button': {
+    config: {
+      contentTypes: [
+        {
+          uid: 'api::home.home',
+          published: {
+            url: 'http://localhost:3000',
+          },
+        },
+        {
+          uid: 'api::page.page',
+          draft: {
+            url: 'http://localhost:3000/api/preview',
+            query: {
+              type: 'page',
+              slug: '{slug}',
+            },
+          },
+          published: {
+            url: 'http://localhost:3000/{slug}',
+          },
+        },
+        {
+          uid: 'api::post.post',
+          draft: {
+            url: 'http://localhost:3000/api/preview',
+            query: {
+              type: 'post',
+              slug: '{slug}',
+            },
+          },
+          published: {
+            url: 'http://localhost:3000/blog/{slug}',
+          },
+        },
+      ],
+    },
+  },
+};
